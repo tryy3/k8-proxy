b465b858c16e209bd7e416ca768724068af176ce
tryy3 <dennistryy3@gmail.com>
2025-08-19 14:26:37 +0200

    Tsnet hello world test

--- main.go
+++ main.go
@@ -1,1 +1,58 @@
+// This program demonstrates how to use tsnet as a library.
 package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"strings"
+
+	"tailscale.com/tsnet"
+)
+
+var (
+	addr = flag.String("addr", ":80", "address to listen on")
+)
+
+func main() {
+	flag.Parse()
+	srv := new(tsnet.Server)
+	defer srv.Close()
+	ln, err := srv.Listen("tcp", *addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+
+	lc, err := srv.LocalClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *addr == ":443" {
+		ln = tls.NewListener(ln, &tls.Config{
+			GetCertificate: lc.GetCertificate,
+		})
+	}
+
+	log.Fatal(http.Serve(ln, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		who, err := lc.WhoIs(r.Context(), r.RemoteAddr)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		fmt.Fprintf(w, "<html><body><h1>Hello, world!</h1>\n")
+		fmt.Fprintf(w, "<p>You are <b>%s</b> from <b>%s</b> (%s)</p>",
+			html.EscapeString(who.UserProfile.LoginName),
+			html.EscapeString(firstLabel(who.Node.ComputedName)),
+			r.RemoteAddr)
+	})))
+}
+
+func firstLabel(s string) string {
+	s, _, _ = strings.Cut(s, ".")
+	return s
+}
